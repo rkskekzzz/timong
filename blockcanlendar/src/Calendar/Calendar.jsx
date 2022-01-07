46e12da8a085ea5e414ae117d5627b04aac43dd8
su <wkdlfflxh@naver.com>
2022-01-08 04:46:18 +0900

    styled, calendar view update

--- blockcanlendar/src/Calendar/Calendar.jsx
+++ blockcanlendar/src/Calendar/Calendar.jsx
@@ -2,10 +2,22 @@ import React, { useState } from 'react';
 import moment from 'moment';
 import styled from 'styled-components';
 
+const users = [
+  { name: 'ycha', color: 'red', avail: [1, 2, 3] },
+  { name: 'suhshin', color: 'blue', avail: [2] },
+  { name: 'ycha', color: '#aabb33', avail: [1, 2, 3] },
+  { name: 'suhshin', color: '#FF0000', avail: [2] },
+  { name: 'ycha', color: '#FF0000', avail: [1, 2, 3] },
+  { name: 'suhshin', color: '#FF0000', avail: [2] },
+];
+
+const color = ['red', 'blue', 'black'];
+
 const Calendar = () => {
   const [getMoment, setMoment] = useState(moment());
 
   const today = getMoment;
+  let i = 0;
   const firstWeek = today.clone().startOf('month').week();
   const lastWeek =
     today.clone().endOf('month').week() === 1
@@ -20,6 +32,7 @@ const Calendar = () => {
   const calendarArr = () => {
     let result = [];
     let week = firstWeek;
+
     for (week; week <= lastWeek; week++) {
       result = result.concat(
         <HFlexBox key={week}>
@@ -33,7 +46,7 @@ const Calendar = () => {
                 .week(week)
                 .startOf('week')
                 .add(index, 'day'); //d로해도되지만 직관성
-
+              let isThisMonth = days.month() !== today.month() ? false : true;
               return (
                 <CalendarBox
                   key={index}
@@ -41,7 +54,17 @@ const Calendar = () => {
                     handleClick(days);
                   }}
                 >
-                  <CalendarDateLabel>{days.format('D')}</CalendarDateLabel>
+                  <CalendarDateLabel isThisMonth={isThisMonth}>
+                    {days.format('D')}
+                  </CalendarDateLabel>
+                  <CalendarDateCircleBox isThisMonth={isThisMonth}>
+                    <Circle size="small" color={color[i++ % 3]} />
+                    <Circle size="small" color={color[i++ % 3]} />
+                    <Circle size="small" color={color[i++ % 3]} />
+                    <Circle size="small" color={color[i++ % 3]} />
+                    <Circle size="small" color={color[i++ % 3]} />
+                    <Circle size="small" color={color[i++ % 3]} />
+                  </CalendarDateCircleBox>
                 </CalendarBox>
               );
             })}
@@ -52,12 +75,29 @@ const Calendar = () => {
   };
 
   return (
-    <MainComponent>
-      <HScrollBox>
-        <div>a</div>
-      </HScrollBox>
-      <VFlexBox>{calendarArr()}</VFlexBox>
-    </MainComponent>
+    <>
+      <TitleComponent>
+        <div>TitleLabel</div>
+      </TitleComponent>
+      <MainComponent>
+        <BottomShadowBox>
+          <HScrollBox>
+            {users.map((e) => {
+              return (
+                <HFlexUserBox>
+                  <Circle color={e.color} />
+                  <UserNameLabel>{e.name}</UserNameLabel>
+                </HFlexUserBox>
+              );
+            })}
+          </HScrollBox>
+        </BottomShadowBox>
+        <CalendarPaddingBox>
+          <VFlexBox>{calendarArr()}</VFlexBox>
+          <VFlexBox>{calendarArr()}</VFlexBox>
+        </CalendarPaddingBox>
+      </MainComponent>
+    </>
   );
 };
 export default Calendar;
@@ -81,13 +121,38 @@ export default Calendar;
 //       </div>
 
 const MainComponent = styled.div`
+  max-width: 1024px;
   display: flex;
   flex-direction: column;
-  padding: 10%;
+  gap: 20px;
+`;
+const BottomShadowBox = styled.div`
+  background: white;
+  box-shadow: 0px 3px 2px 1px #dadada;
+  position: sticky;
+  top: 0;
+`;
+
+const TitleComponent = styled.div`
+  background-color: #679fff;
+  height: 55px;
+  display: flex;
+`;
+
+const CalendarPaddingBox = styled.div`
+  padding: 0 20px;
 `;
 
 const HScrollBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
   overflow: scroll;
+  padding: 10px;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  // background: #25a5b3;
 `;
 
 const VFlexBox = styled.div`
@@ -97,16 +162,70 @@ const VFlexBox = styled.div`
 `;
 
 const HFlexBox = styled.div`
-  width: 100%;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  align-items: center;
+  justify-content: stretch;
+`;
+
+const HFlexUserBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  padding: 2px 5px;
+  border-radius: 12px;
+  background: #f0f0f0;
 `;
 
 const CalendarBox = styled.div`
-  border: 1px solid blue;
+  flex-basis: 200px;
+
+  padding: 1%;
 `;
 
 const CalendarDateLabel = styled.div`
   width: 20px;
+
+  ${({ isThisMonth }) => {
+    if (!isThisMonth) {
+      return `color: white;`;
+    }
+  }};
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+
+  ${({ size }) => {
+    if (size === 'small') {
+      return `width: 9px;
+        height: 9px;`;
+    } else {
+      return `width: 11px;
+      height: 11px`;
+    }
+  }};
+
+  ${({ color }) => {
+    return `background: ${color}`;
+  }};
+`;
+
+const UserNameLabel = styled.span`
+  font-size: 1.2rem;
+`;
+
+const CalendarDateCircleBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3px;
+  justify-content: flex-start;
+  padding: 5px 0;
+  border-bottom: 1px solid gray;
+  ${({ isThisMonth }) => {
+    if (!isThisMonth) {
+      return `display: none`;
+    }
+  }};
 `;
