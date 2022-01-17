ada0b877f4998e232e621b35c6d52696ba99649e
rkskekzzz <wkdlfflxh@naver.com>
2022-01-17 18:01:45 +0900

    Feat : X 모양 생성

--- frontend/src/Components/Styled/global.styled.ts
+++ frontend/src/Components/Styled/global.styled.ts
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
 type CircleType = 'small';
+
 const Circle = styled.div<{ size?: CircleType }>`
-  // margin: 0 auto;
-  // border-radius: 50%;
   // width: ${(props) => (props.size === 'small' ? '9px' : '11px')};
   // height: ${(props) => (props.size === 'small' ? '9px' : '11px')};
   width: 100%;
@@ -12,6 +11,35 @@ const Circle = styled.div<{ size?: CircleType }>`
   background: ${(props) => props.color || 'white'};
 `;
 
+const Xone = styled.div<{ color?: string }>`
+  width: 100%;
+  height: 100%;
+  margin-left: 3px;
+
+  &: before {
+    position: absolute;
+    content: ' ';
+    height: 9px;
+    width: 2px;
+    background: ${(props) => props.color || 'white'};
+  }
+  &: after {
+    position: absolute;
+    content: ' ';
+    height: 9px;
+    width: 2px;
+    background: ${(props) => props.color || 'white'};
+  }
+
+  &: before {
+    transform: rotate(45deg);
+  }
+  &: after {
+    transform: rotate(-45deg);
+  }
+`;
+
 export default {
   Circle,
+  Xone,
 };
