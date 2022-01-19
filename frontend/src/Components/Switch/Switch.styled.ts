2dedade5424cc7d2e5e7e074244214e1b34c9a2e
rkskekzzz <wkdlfflxh@naver.com>
2022-01-20 02:31:06 +0900

    Feat : Switch 테마 추가

--- frontend/src/Components/Switch/Switch.styled.ts
+++ frontend/src/Components/Switch/Switch.styled.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
-const Switch = styled.div`
+const Switch = styled.div<{ bgcolor: string }>`
+  height: 40px;
+  margin: 8px 0;
   position: fixed;
-  bottom: 30px;
+  bottom: 16px;
   left: 50%;
   transform: translateX(-50%);
-  background: #dddddd;
-  background: #fafafaaa;
+  background: ${(props) => props.bgcolor || '#fafafadf'};
   border-radius: 50px;
   box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%),
     0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
@@ -15,17 +16,42 @@ const SwitchInput = styled.input`
   display: none;
 `;
 const SwitchLabel = styled.label`
-  width: 8rem;
+  width: 6rem;
   position: relative;
   display: flex;
-  padding: 0.4rem 0.6rem;
+  padding: 0.4rem 0.3rem;
 `;
 
-const SwitchText = styled.div`
+const SwitchText = styled.div<{ color: string }>`
   flex: 1 1 0px;
-  font-size: 0.8rem;
+  font-size: 1rem;
   text-align: center;
+  font-weight: bold;
+  color: ${(props) => props.color || '#fafafadf'};
 `;
-const Styled = { Switch, SwitchLabel, SwitchInput, SwitchText };
+
+const SwitchSelected = styled.div<{ isChecked: boolean }>`
+  margin: 0.1rem 0.15rem;
+  width: calc(55% - 0.3rem);
+  height: calc(100% - 0.2rem);
+  border-radius: 50px;
+  position: absolute;
+  background: #f995f0b8;
+  box-shadow: inset 0px 2px 1px -1px #f995f0b8, inset 0px 1px 1px 0px #f995f0a8,
+    inset 0px 1px 3px 0px #f995f098;
+  ${(props) => {
+    if (!props.isChecked)
+      return `
+        transition: transform 200ms ease-in-out 0ms;
+        transform: translateX(calc(3.6rem - 0.6rem));
+      `;
+    else
+      return `
+        transition: transform 200ms ease-in-out 0ms;
+        `;
+  }};
+`;
+
+const Styled = { Switch, SwitchLabel, SwitchInput, SwitchText, SwitchSelected };
 
 export default Styled;
--- frontend/src/Components/Switch/Switch.tsx
+++ frontend/src/Components/Switch/Switch.tsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import Styled from './Switch.styled';
+import { ThemeContext } from '../Timong';
 
-const Switch = () => {
-  const [isChecked, setIsChecked] = useState<boolean>(true);
-
-  const handleToggle = () => setIsChecked(!isChecked);
+const Switch: React.FC<{
+  isChecked: boolean;
+  handleToggle: () => void;
+}> = ({ isChecked, handleToggle }) => {
+  const theme = useContext(ThemeContext);
 
   return (
-    <Styled.Switch>
+    <Styled.Switch bgcolor={theme.backgroundSwitch}>
       <Styled.SwitchInput
         type="checkbox"
         className={`switch-checkbox`}
-        checked={isChecked}
         onChange={handleToggle}
+        checked={isChecked}
         id={`switch-input`}
       />
+      <Styled.SwitchSelected isChecked={isChecked}></Styled.SwitchSelected>
       <Styled.SwitchLabel htmlFor={`switch-input`}>
-        <Styled.SwitchText>되는 날</Styled.SwitchText>
-        <Styled.SwitchText>안되는 날</Styled.SwitchText>
+        <Styled.SwitchText color={theme.foregroundSwitch}>O</Styled.SwitchText>
+        <Styled.SwitchText color={theme.foregroundSwitch}>X</Styled.SwitchText>
       </Styled.SwitchLabel>
     </Styled.Switch>
   );
