8ebec56999798a9c4134ee2730389b232002e647
rkskekzzz <wkdlfflxh@naver.com>
2022-01-24 20:28:34 +0900

    Feat : react-vertualized 적용 완료

--- frontend/src/Components/Calendar/Calendar.tsx
+++ frontend/src/Components/Calendar/Calendar.tsx
@@ -14,7 +14,7 @@ import { Year, Day } from 'src/Interface/DateType';
 import { buildDate } from 'src/Utils';
 import { User, Valid } from 'src/Interface/UserType';
 
-// import { AutoSizer, List } from 'react-virtualized';
+import { AutoSizer, List, WindowScroller } from 'react-virtualized';
 type UserWithValid = {
   info: User;
   valid: Valid;
@@ -27,6 +27,7 @@ const Calendar = () => {
   const [selectedDay, setSelectedDay] = useState<Day | null>(null);
 
   const touchRef = useRef(null);
+  const nodeRef = useRef(null);
   const [isShow, setIsShow] = useState<boolean>(false);
   const [dayUsers, setDayUsers] = useState<UserWithValid[]>([]);
   const handleDrawerOpen = () => setIsShow(!isShow);
@@ -89,17 +90,66 @@ const Calendar = () => {
     setSelectedDay,
   };
 
+  const scrollListener = (params) => {
+    if (params.scrollTop + params.clientHeight >= params.scrollHeight - 300) {
+      console.log('지금!');
+
+      setYear([
+        ...year,
+        ...buildDate(year[year.length - 1].monthMoment.clone().add(1, 'M')),
+      ]);
+    }
+  };
+  const rowRenderer = ({ index, style }) => {
+    const month = year[index];
+    return (
+      <div style={style} key={month.monthMoment.format('x') + index}>
+        <MonthBox month={month} drawerHandler={drawerHandler} />
+      </div>
+    );
+  };
+
+  const getRowHeight = ({ index }) => {
+    return 60 + year[index].week.length * 80;
+  };
+
+  /**
+   *  WindowScroller props 의미
+   * @param width : 너비
+   * @param height: 높이
+   * ㄴ 그려줄 data grid의 전체 사이즈라고 생각하면 편하다
+   * @param isScrolling : 스크롤을 감지하는 상태값이다.
+   * @param scrollTop : scrollTop을 바꿀 수 있는 변수 해당 값을 바꾸면 원하는 위치로 이동할 수 있다.
+   * @param registerChild : registerChild를 활용해서 windowscroller의 자식 컴포넌트로 ref를 전달할 수 있다.
+   * ref를 사용하지 않으면 windowscroller가 findDOMNode()를 호출하는데 이는 strict mode에서는 권장하지 않는 방식이기 때문에 ref를 사용하는 것이 적합하다.
+   * 문제인지
+   * @link https://github.com/bvaughn/react-virtualized/issues/1572
+   * 해결책
+   * @link https://github.com/bvaughn/react-virtualized/blob/master/docs/WindowScroller.md#render-props
+   */
   return (
     <>
-      <Styled.CalendarPaddingBox color={theme.foreground}>
-        {year.map((month) => (
-          <MonthBox
-            key={month.monthMoment.format('MMMM')}
-            month={month}
-            drawerHandler={drawerHandler}
-          />
-        ))}
-      </Styled.CalendarPaddingBox>
+      <WindowScroller nodeRef={nodeRef}>
+        {({ width, height, isScrolling, scrollTop, registerChild }) => (
+          <div ref={registerChild}>
+            <AutoSizer>
+              {() => (
+                <List
+                  autoHeight
+                  height={height}
+                  isScrolling={isScrolling}
+                  onScroll={scrollListener}
+                  rowCount={year.length}
+                  rowHeight={getRowHeight}
+                  rowRenderer={rowRenderer}
+                  scrollTop={scrollTop}
+                  width={width}
+                />
+              )}
+            </AutoSizer>
+          </div>
+        )}
+      </WindowScroller>
       <Styled.UserDrawer
         anchor="bottom"
         open={isShow}
@@ -120,35 +170,3 @@ const Calendar = () => {
 };
 
 export default React.memo(Calendar);
-
-// const scrollListener = (params) => {
-//   if (params.scrollTop + params.clientHeight >= params.scrollHeight - 300) {
-//     setYear([
-//       ...year,
-//       ...buildDate(year[year.length - 1].monthMoment.clone().add(1, 'M')),
-//     ]);
-//   }
-// };
-// const rowRanderer = ({ index, style }) => {
-//   const month = year[index];
-//   return (
-//     <div style={style}>
-//       <MonthBox key={month.monthMoment.format('MMMM')} month={month} />
-//     </div>
-//   );
-// };
-{
-  /* <AutoSizer>
-      {({ width }) => (
-        <List
-          rowCount={year.length}
-          height={1000}
-          rowHeight={600}
-          width={width}
-          rowRenderer={rowRanderer}
-          onScroll={scrollListener}
-          overscanRowCount={5}
-        />
-      )}
-    </AutoSizer> */
-}
