b4e8654bf0326ac1419a21029b243dd0ad9428b5
rkskekzzz <wkdlfflxh@naver.com>
2022-02-10 17:15:13 +0900

    Style : arrow image added

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,25 +1,25 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 import {
   CalendarController,
   UserController,
   ScheduleController,
-} from "../controllers";
+} from '../controllers';
 
 const router: Router = Router();
 
 // 📆 캘린더 라우터
-router.post("/", CalendarController.create);
-router.get("/:calendar_id", CalendarController.getOne);
-router.put("/:calendar_id", CalendarController.update);
+router.post('/', CalendarController.create);
+router.get('/:calendar_id', CalendarController.getOne);
+router.put('/:calendar_id', CalendarController.update);
 
 // 🙍‍♂️ 유저 라우터
-router.post("/:calendar_id/users", UserController.create);
-router.put("/:calendar_id/users", UserController.update);
-router.get("/:calendar_id/users", UserController.getAll);
-router.delete("/:calendar_id/users/:user_id", UserController.remove);
+router.post('/:calendar_id/users', UserController.create);
+router.put('/:calendar_id/users', UserController.update);
+router.get('/:calendar_id/users', UserController.getAll);
+router.delete('/:calendar_id/users/:user_id', UserController.remove);
 
 // 🎏 스케쥴 라우터
-router.post("/:calendar_id/users/:user_id/schedule", ScheduleController.update);
+router.post('/:calendar_id/users/:user_id/schedule', ScheduleController.update);
 
 export default router;
