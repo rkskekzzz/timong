fa0463820c4f371877bf89026608a115f9be04c6
rkskekzzz <wkdlfflxh@naver.com>
2022-02-12 02:59:33 +0900

    Feat : 에러페이지에서 아무 곳이나 누르면 홈으로 가는 기능 추가

--- frontend/src/Components/Error/Error.tsx
+++ frontend/src/Components/Error/Error.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Styled from './Error.styled';
 import GlobalStyled from '../GlobalStyled.styled';
+import { useNavigate } from 'react-router-dom';
 
 const Error = () => {
+  const navi = useNavigate();
   return (
-    <Styled.ErrorBox>
+    <Styled.ErrorBox onClick={() => navi('/')}>
       <span>
         <b>T</b>
         <i>T</i>
@@ -22,10 +24,12 @@ const Error = () => {
         <i>!</i>
       </span>
       <div className="flex">
-        <h1>404</h1>
+        <h1>
+          40<b>4</b>
+        </h1>
         <h3>Page Not Found..</h3>
+        <h4>Press anywhere to Home</h4>
       </div>
-
       <GlobalStyled.Cloud bgcolor="#ffd8fb">
         <div className="clouds">
           <div className="cloud x1" />
