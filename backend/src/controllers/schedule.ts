3bce8e852292fdaca3446b20e7ed4a5a1d76b555
huni <petercha2000@gmail.com>
2022-02-13 16:22:24 +0900

    Fix: 스케쥴 변경 응답 body 삭제
    - 프론트에서 state 코드만 필요하기 떄문에 삭제함

--- backend/src/controllers/schedule.ts
+++ backend/src/controllers/schedule.ts
@@ -7,13 +7,10 @@ async function update(req: Request, res: Response): Promise<Response> {
   const calendar_id: string = req.params.calendar_id;
   const user_id: string = req.params.user_id;
   const updateSchduleDTO: UpdateSchduleDTO = req.body;
-  const calendar = await ScheduleService.update(
-    calendar_id,
-    user_id,
-    updateSchduleDTO
-  );
 
-  return res.status(200).json(calendar);
+  await ScheduleService.update(calendar_id, user_id, updateSchduleDTO);
+
+  return res.sendStatus(200);
 }
 
 export const ScheduleController = {
