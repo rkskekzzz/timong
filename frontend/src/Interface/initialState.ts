89e2dc8a612770dba2c5cbaaa59b6501c7f6aa9e
rkskekzzz <wkdlfflxh@naver.com>
2022-03-08 16:40:17 +0900

    Fix : initialState 수정

--- frontend/src/Interface/initialState.ts
+++ frontend/src/Interface/initialState.ts
@@ -1,8 +1,7 @@
-import moment from 'moment';
 import { Day } from './DateType';
 import { User } from './UserType';
 
 export const initialState: { users: User[]; selectedDate: Day } = {
   users: [],
-  selectedDate: new Day(moment()),
+  selectedDate: null,
 };
