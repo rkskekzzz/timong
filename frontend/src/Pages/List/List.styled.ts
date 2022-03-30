7db21ba99bc53314324e2f819b5f657771aa1007
rkskekzzz <wkdlfflxh@naver.com>
2022-03-31 01:22:07 +0900

    Feat : List component

---
+++ frontend/src/Pages/List/List.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const List = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const Styled = { List };
+
+export default Styled;
---
+++ frontend/src/Pages/List/List.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Styled from './List.styled';
+
+const List = () => {
+  return <Styled.List>List</Styled.List>;
+};
+
+export default List;
