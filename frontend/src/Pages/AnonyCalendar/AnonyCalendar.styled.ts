4f6da3e273e40cc916fa51de2a2281e3607178ed
rkskekzzz <wkdlfflxh@naver.com>
2022-03-31 23:18:37 +0900

    Refactor : 기본 캘린더를 AnonyCalendar로 수정

--- frontend/src/Pages/AnonyCalendar/AnonyCalendar.styled.ts
+++ frontend/src/Pages/AnonyCalendar/AnonyCalendar.styled.ts
@@ -1,13 +1,21 @@
 import styled from 'styled-components';
 import NumberEx from 'src/Common/NumberEx';
 
+const AnonyCalendar = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 60px);
+  overflow: hidden;
+`;
+
 const Body = styled.div`
+  flex: 1 1 0;
   width: 100vw;
   max-width: ${NumberEx.calendarMaxWidth};
   margin: 0 auto;
   padding-top: 60px;
 `;
 
-const Styled = { Body };
+const Styled = { AnonyCalendar, Body };
 
 export default Styled;
--- frontend/src/Pages/AnonyCalendar/AnonyCalendar.tsx
+++ frontend/src/Pages/AnonyCalendar/AnonyCalendar.tsx
@@ -4,79 +4,25 @@ import Styled from './AnonyCalendar.styled';
 import { Calendar } from 'src/Components/Calendar';
 import Header from 'src/Components/Header';
 import Users from 'src/Components/Users';
-import { themes } from 'src/theme';
 import { CalendarService } from 'src/Network/CalendarService';
 import CircularProgress from '@mui/material/CircularProgress';
 import Button from '@mui/material/Button';
 import { UserContext } from 'src/App';
 import { useLocation } from 'react-router-dom';
-import { useMediaQuery } from '@mui/material';
 import { CalendarType } from 'src/Interface/CalendarType';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-declare module '@mui/material/styles' {
-  interface Theme {
-    myPalette: {
-      mode: string;
-      foregroundHeader: string;
-      backgroundHeader: string;
-      backgroundModal: string;
-      backDropHeader: string;
-      foreground: string;
-      iconSmall: string;
-      icon: string;
-      foregroundSwitch: string;
-      backgroundSwitch: string;
-      foregroundAddButton: string;
-      backgroundAddButton: string;
-      backDrop: string;
-      background: string;
-    };
-  }
-  interface ThemeOptions {
-    myPalette?: {
-      mode?: string;
-      foregroundHeader?: string;
-      backgroundHeader?: string;
-      backgroundModal?: string;
-      backDropHeader?: string;
-      foreground?: string;
-      iconSmall?: string;
-      icon?: string;
-      foregroundSwitch?: string;
-      backgroundSwitch?: string;
-      foregroundAddButton?: string;
-      backgroundAddButton?: string;
-      backDrop?: string;
-      background?: string;
-    };
-  }
-}
 
 const AnonyCalendar = () => {
   const location = useLocation();
   const navi = useNavigate();
   const { dispatch } = useContext(UserContext);
-  // const [calendarType, setCalendarType] = useState<CalendarType>('Monthly');
   const calendarType: CalendarType = 'Monthly';
-  const [themeChanged, setThemeChanged] = useState<boolean>(false);
   const [reLoad, setReLoad] = useState<boolean>(false);
   const [calendarName, setCalendarName] = useState<string>('');
   const [calendar, setCalendar] = useState<boolean>(false);
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const [mode, setMode] = useState<string>(prefersDarkMode ? 'dark' : 'light');
-  const toggleMode = () => {
-    const _mode = mode === 'dark' ? 'light' : 'dark';
-    setThemeChanged(true);
-    setMode(_mode);
-  };
 
   const getCalendar = async () => {
-    const result = await CalendarService.getCalendar(
-      '/' + location.pathname.split('/')[2]
-    );
+    const result = await CalendarService.getCalendar(location.pathname);
     console.log(result);
-
     if (result) {
       dispatch({
         type: 'INIT',
@@ -90,29 +36,6 @@ const AnonyCalendar = () => {
     }
   };
 
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: (
-            themeChanged ? (mode === 'dark' ? false : true) : prefersDarkMode
-          )
-            ? 'dark'
-            : 'light',
-        },
-        myPalette: (
-          themeChanged ? (mode === 'dark' ? false : true) : prefersDarkMode
-        )
-          ? themes.dark
-          : themes.light,
-      }),
-    [prefersDarkMode, mode, themeChanged]
-  );
-
-  useEffect(() => {
-    document.body.style.background = theme.myPalette.background;
-  }, [theme]);
-
   useEffect(() => {
     if (reLoad || calendar) return;
     getCalendar();
@@ -125,37 +48,35 @@ const AnonyCalendar = () => {
   }, []);
 
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Header toggleMode={toggleMode} calendarName={calendarName} />
-        {!calendar ? (
-          <div
-            style={{
-              width: '100%',
-              height: '100vh',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            {reLoad ? (
-              <Button size="large" onClick={() => window.location.reload()}>
-                Refresh!
-              </Button>
-            ) : (
-              <CircularProgress />
-            )}
-          </div>
-        ) : (
-          <>
-            <Styled.Body className="here">
-              <Calendar calendarType={calendarType} />
-              <Users />
-            </Styled.Body>
-          </>
-        )}
-      </ThemeProvider>
-    </>
+    <Styled.AnonyCalendar>
+      <Header calendarName={calendarName} />
+      {!calendar ? (
+        <div
+          style={{
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {reLoad ? (
+            <Button size="large" onClick={() => window.location.reload()}>
+              Refresh!
+            </Button>
+          ) : (
+            <CircularProgress />
+          )}
+        </div>
+      ) : (
+        <>
+          <Styled.Body className="here">
+            <Calendar calendarType={calendarType} />
+            <Users />
+          </Styled.Body>
+        </>
+      )}
+    </Styled.AnonyCalendar>
   );
 };
 
