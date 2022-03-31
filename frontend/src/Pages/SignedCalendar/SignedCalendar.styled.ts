c26434878f570b07f1f1d16ca772ca53b8aed30e
rkskekzzz <wkdlfflxh@naver.com>
2022-03-31 14:18:19 +0900

    Refactor : List -> SignedCalendar

---
+++ frontend/src/Pages/SignedCalendar/SignedCalendar.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const SignedCalendar = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const Styled = { SignedCalendar };
+
+export default Styled;
---
+++ frontend/src/Pages/SignedCalendar/SignedCalendar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Styled from './SignedCalendar.styled';
+
+const SignedCalendar = () => {
+  return <Styled.SignedCalendar>SignedCalendar</Styled.SignedCalendar>;
+};
+
+export default SignedCalendar;
