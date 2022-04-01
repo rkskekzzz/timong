bc5866c29e84b076336c2704dea2af31c142aedc
rkskekzzz <wkdlfflxh@naver.com>
2022-04-01 23:09:02 +0900

    Feat : List Component Added

--- frontend/src/Pages/SignedCalendar/List/List.styled.ts
+++ frontend/src/Pages/SignedCalendar/List/List.styled.ts
@@ -14,11 +14,21 @@ const List = styled.div`
   }
 `;
 
-const Card = styled.span`
+const Card = styled.span<{ fgcolor: string }>`
   width: 40vw;
   height: 100px;
-  background: #ffffff66;
+  padding: 1rem;
+  background: #ffffff33;
   border-radius: 10px;
+  color: ${(props) => `${props.fgcolor}`};
+  box-shadow: 0px 4px 2px -2px rgb(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgb(0, 0, 0, 0.14), 0px 2px 6px 0px rgb(0, 0, 0, 0.12);
+
+  h2,
+  p {
+    padding: 0;
+    margin: 0;
+  }
 `;
 
 const Styled = { List, Card };
--- frontend/src/Pages/SignedCalendar/List/List.tsx
+++ frontend/src/Pages/SignedCalendar/List/List.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import Styled from './List.styled';
+import { Group } from 'src/Interface/Group';
+import { useTheme } from '@mui/material';
 
-const Card = () => {
-  return <Styled.Card>card</Styled.Card>;
+const arr: Group[] = [
+  { group_name: 'hi', group_users: 3, group_id: '1' },
+  { group_name: 'asdf', group_users: 1, group_id: '2' },
+  { group_name: 'qwer', group_users: 2, group_id: '3' },
+];
+
+const selectedStyle = {
+  background: 'red',
+};
+
+const Card = ({ group }: { group: Group }) => {
+  const theme = useTheme();
+  return (
+    <Styled.Card fgcolor={theme.myPalette.foreground}>
+      <h2>{group.group_name}</h2>
+      <p>{group.group_users}</p>
+    </Styled.Card>
+  );
 };
 
 const List = () => {
   return (
     <Styled.List>
       <div className="list-box">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {arr.map((element) => {
+          return <Card key={element.group_id} group={element} />;
+        })}
       </div>
     </Styled.List>
   );
