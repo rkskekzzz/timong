b43ef0f831868aabd747c8854ba996057822eacb
rkskekzzz <wkdlfflxh@naver.com>
2022-04-09 17:37:25 +0900

    Refactor : 주석 제거

--- frontend/src/Pages/Enterence/SignInButton/SignInButton.tsx
+++ frontend/src/Pages/Enterence/SignInButton/SignInButton.tsx
@@ -15,29 +15,15 @@ import { UserContext } from 'src/App';
 const provider = new GoogleAuthProvider();
 
 const SignInButton = () => {
-  const { dispatch } = useContext(UserContext);
+  const { state, dispatch } = useContext(UserContext);
   const navi = useNavigate();
   const asdf = () => {
     signInWithRedirect(auth, provider)
       .then((result: any) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // const credential = GoogleAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
-        // The signed-in user info.
-        // const user = result.user;
-        // ...
         console.log(result);
       })
       .catch((error) => {
         console.log(error);
-        // Handle Errors here.
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // The email of the user's account used.
-        // const email = error.email;
-        // The AuthCredential type that was used.
-        // const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
       });
   };
   const zxcv = () => {
@@ -46,16 +32,9 @@ const SignInButton = () => {
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        dispatch({ type: 'SIGNIN' });
-        console.log(user);
-        // navi('/calendar');
+        dispatch({ type: 'SIGNIN', uid: user.uid });
+        navi('/calendar');
       }
-      // This gives you a Google Access Token. You can use it to access Google APIs.
-      // const credential = GoogleAuthProvider.credentialFromResult(result);
-      // const token = credential.accessToken;
-      // // The signed-in user info.
-      // const user = result.user;
-      // console.log(result);
     });
   }, []);
   return (
