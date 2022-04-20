e135b9ae0b3407538c981381973486cf4f46ccbf
rkskekzzz <wkdlfflxh@naver.com>
2022-04-21 05:08:52 +0900

    Style : 카드 스타일 지정

--- frontend/src/Pages/SignedCalendar/List/List.styled.ts
+++ frontend/src/Pages/SignedCalendar/List/List.styled.ts
@@ -25,11 +25,23 @@ const Card = styled.span<{ fgcolor: string }>`
   color: ${(props) => `${props.fgcolor}`};
   box-shadow: 0px 4px 2px -2px rgb(0, 0, 0, 0.2),
     0px 2px 2px 0px rgb(0, 0, 0, 0.14), 0px 2px 6px 0px rgb(0, 0, 0, 0.12);
-
+  cursor: pointer;
+  :hover {
+  }
   h2,
   p {
     padding: 0;
     margin: 0;
+  }
+  div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    span {
+      font-size: 3rem;
+    }
   }
 `;
 
