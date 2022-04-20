bd626ae3899fa7d6b12cbe01264c7e9f8e3d2c12
rkskekzzz <wkdlfflxh@naver.com>
2022-04-21 05:08:16 +0900

    Feat : Calendar Type

--- frontend/src/Interface/CalendarType.ts
+++ frontend/src/Interface/CalendarType.ts
@@ -1,4 +1,14 @@
+import { User } from './UserType';
 const Monthly = 'Monthly' as const;
 const Weekly = 'Weekly' as const;
 
 export type CalendarType = typeof Monthly | typeof Weekly;
+
+export type Calendar = {
+  _id?: string;
+  name: string;
+  start?: string;
+  end?: string;
+  users: User[];
+  meetingDays: string[];
+};
