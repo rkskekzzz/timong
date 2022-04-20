40caf0cbc610c6352609445ff14f154b3f5e3da8
rkskekzzz <wkdlfflxh@naver.com>
2022-04-21 05:09:13 +0900

    Refactor : 유저 인증 부분 리스트에서 상위 컴포넌트로 이동

--- frontend/src/Pages/SignedCalendar/SignedCalendar.tsx
+++ frontend/src/Pages/SignedCalendar/SignedCalendar.tsx
@@ -1,19 +1,25 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Styled from './SignedCalendar.styled';
 import { Calendar } from 'src/Components/Calendar';
 import { useTheme } from '@mui/material';
 import { UserContext } from 'src/App';
 import List from './List';
 import Header from 'src/Components/Header';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from 'src/firebase';
 
 const SignedCalendar = () => {
-  const { state } = useContext(UserContext);
+  const { state, dispatch } = useContext(UserContext);
+  const [user, setUser] = useState();
   const theme = useTheme();
 
   useEffect(() => {
-    console.log(document.body.offsetWidth);
-    // console.log(state.users);
-  });
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch({ type: 'SIGNIN', uid: user.uid });
+      }
+    });
+  }, []);
 
   return (
     <Styled.SignedCalendar bgcolor={theme.myPalette.background}>
