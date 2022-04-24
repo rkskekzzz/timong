41237034ed2107e67ace0b73689982db61e6c524
rkskekzzz <wkdlfflxh@naver.com>
2022-04-25 06:47:34 +0900

    Refactor : 불필요한 디자인 삭제

--- frontend/src/Pages/SignedCalendar/List/List.styled.ts
+++ frontend/src/Pages/SignedCalendar/List/List.styled.ts
@@ -1,17 +1,11 @@
 import styled from 'styled-components';
 
 const List = styled.div`
-  width: 100%;
-  display: block;
-
-  overflow: scroll;
-  .list-box {
-    padding: 25px 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 25px;
-  }
+  padding: 25px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
 `;
 
 const Styled = { List };
