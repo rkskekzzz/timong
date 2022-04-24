7150643d891bb9c39780083f58b591f343323115
rkskekzzz <wkdlfflxh@naver.com>
2022-04-25 06:45:30 +0900

    Fix : 달력 중앙정렬 이슈 해결

--- frontend/src/Components/Calendar/Month/Monthly.styled.ts
+++ frontend/src/Components/Calendar/Month/Monthly.styled.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const AutoSizerWrapper = styled.div`
   flex: 1 1 0;
+  max-width: 400px;
+  margin: auto;
   height: 100vh;
   .list::-webkit-scrollbar {
     display: none;
