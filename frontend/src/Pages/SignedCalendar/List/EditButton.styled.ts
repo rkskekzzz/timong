a8bdc6822215b9cd24b526ae2979ae969e547d30
rkskekzzz <wkdlfflxh@naver.com>
2022-04-25 05:17:12 +0900

    Feat : EditButton 컴포넌트 생성

---
+++ frontend/src/Pages/SignedCalendar/List/EditButton.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const EditButton = styled.span`
+  position: absolute;
+  color: white;
+  z-index: 1000;
+  bottom: 8px;
+  right: 8px;
+`;
+
+const Styled = { EditButton };
+
+export default Styled;
---
+++ frontend/src/Pages/SignedCalendar/List/EditButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Styled from './EditButton.styled';
+
+const EditButton = () => {
+  return <Styled.EditButton>asdfs</Styled.EditButton>;
+};
+
+export default EditButton;
