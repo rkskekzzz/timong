897bff9a8f2ac8ea21a69ebf9d058a3df6ddb1de
rkskekzzz <wkdlfflxh@naver.com>
2022-06-27 13:55:58 +0900

    Feat : component 이름 변경

--- frontend/src/Components/Calendar/CalendarView.tsx
+++ frontend/src/Components/Calendar/CalendarView.tsx
@@ -3,10 +3,10 @@ import Monthly from './Month';
 import Weekly from './Week';
 import { CalendarType } from 'src/Interface/CalendarType';
 
-const Calendar: React.FC<{
+const CalendarView: React.FC<{
   calendarType: CalendarType;
 }> = ({ calendarType }) => {
   return <>{calendarType === 'Monthly' ? <Monthly /> : <Weekly />}</>;
 };
 
-export default Calendar;
+export default CalendarView;
