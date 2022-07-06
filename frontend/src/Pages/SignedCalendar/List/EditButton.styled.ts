9ca978eda35f04eea34e44aca29865d611d797ee
rkskekzzz <wkdlfflxh@naver.com>
2022-07-06 20:05:49 +0900

    :fire: 불필요한 코드 제거

--- frontend/src/Pages/SignedCalendar/List/EditButton.styled.ts
+++ frontend/src/Pages/SignedCalendar/List/EditButton.styled.ts
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 const EditButton = styled.span`
   color: white;
   z-index: 1000;
-  /* bottom: 16px; */
-  top: calc(100vh - 376px);
-  right: 16px;
 `;
 
 const Styled = { EditButton };
