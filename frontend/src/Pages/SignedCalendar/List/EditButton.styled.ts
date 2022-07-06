9e3c8903abe5c06eb012b8b40888f5e731fe4618
rkskekzzz <wkdlfflxh@naver.com>
2022-07-06 19:55:58 +0900

    🐛 버튼 위치 수정

--- frontend/src/Pages/SignedCalendar/List/EditButton.styled.ts
+++ frontend/src/Pages/SignedCalendar/List/EditButton.styled.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 const EditButton = styled.span`
   color: white;
   z-index: 1000;
-  bottom: 16px;
+  /* bottom: 16px; */
+  top: calc(100vh - 376px);
   right: 16px;
 `;
 
