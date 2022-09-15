6ec2115bd1f5a4858b6fd61bd070adcc649f0408
rkskekzzz <wkdlfflxh@naver.com>
2022-09-16 00:08:21 +0900

    💩 firebase 노출된 key삭제 및 .env참조로 수정

--- frontend/src/firebase.tsx
+++ frontend/src/firebase.tsx
@@ -2,24 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_APIKEY,
-//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-//   projectId: process.env.REACT_APP_PROJECTID,
-//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-//   appId: process.env.REACT_APP_APPID,
-//   measurementId: process.env.REACT_APP_MEASUREMENTID,
-// };
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyBD8KMJk7-q9_0CqLhoWK4-SinAQ4nT-3Y',
-  authDomain: 'timong-a2e9b.firebaseapp.com',
-  projectId: 'timong-a2e9b',
-  storageBucket: 'timong-a2e9b.appspot.com',
-  messagingSenderId: '1026357510086',
-  appId: '1:1026357510086:web:6f1545039f7242519dbcc7',
-  measurementId: 'G-DDH2BYWN3S',
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
 export const app = initializeApp(firebaseConfig);
