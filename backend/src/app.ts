8c3c783890409f57e28b53fa09d94528f24d5b6c
rkskekzzz <wkdlfflxh@naver.com>
2023-05-03 20:15:46 +0900

    🐛 cors bug fixed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,32 +1,36 @@
-import express from "express";
+import express from 'express';
 
-import cors from "cors";
-import logger from "morgan";
+import cors from 'cors';
+import logger from 'morgan';
 
-import config from "./config";
-import routes from "./routes";
-import * as error from "./modules/error";
-import { mongooseConnect } from "./db/connent";
+import config from './config';
+import routes from './routes';
+import * as error from './modules/error';
+import { mongooseConnect } from './db/connent';
 
 const app = express();
 
+let corsOptions = {
+    origin: true,
+};
+
 app.use(express.json());
-app.use(cors());
-app.use(logger("dev"));
+app.use(cors(corsOptions));
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/calendar", routes);
+app.use('/calendar', routes);
 
 app.use(error.errorPageNotFound);
 app.use(error.errorHandler);
 
 mongooseConnect().then(() => {
-  console.log(`DB connected!`);
-  app.listen(config.host.port, () => {
-    console.log(`
+    console.log(`DB connected!`);
+    app.listen(config.host.port, () => {
+        console.log(`
       ################################################
       🛡️  Server listening on port: ${config.host.port}🛡️
       ################################################
     `);
-  });
+    });
 });
